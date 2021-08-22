8edd0f4ccf66b9f03371e3e0607a34c0879a8e41
刘伟 <lemonpaimc@126.com>
2021-08-23 07:15:36 +0800

    feat: 日志模块新增

--- packages/samurais-log/src/interface.ts
+++ packages/samurais-log/src/interface.ts
@@ -1,8 +1,15 @@
 // 异常类型
 export type CatchType = "promise" | "fetch" | "error" | "empty";
 
+export type InfoType = "info" | "message";
+
 // 异常信息
 export type ICatch = {
     type: CatchType;
+    message: Record<string, unknown> | string;
+}
+
+export type IMessage = {
+    type: InfoType;
     message: Record<string, unknown> | string;
 }
--- packages/samurais-log/src/logger.ts
+++ packages/samurais-log/src/logger.ts
@@ -1,14 +1,44 @@
 import { isEmpty, isObject, isString } from 'samurais-utils';
-import { ICatch } from "./interface";
+import { ICatch, IMessage } from "./interface";
+
+
+const tagLevelColor = new Map([
+    ['success', '#03913C'],
+    ['env', '#1A83EC'],
+    ['info', '#06A598'],
+    ['error', '#E40662']
+]);
+export type LOGTYPE = "success" | "info" | "error" | 'env';
 
 export class Logger {
+
+    private static log(level: LOGTYPE, label: string, value: unknown) {
+        if (!window || !window.console) return;
+        console.log(
+            `%c${label}%c${value}`,
+            "background: #6f5a5a; color: #fff;border-top-left-radius: 2px; border-bottom-left-radius: 2px; padding: 2px 10px;",
+            `background: ${tagLevelColor.get(
+                level,
+            )};color: #fff;border-top-right-radius: 2px; border-bottom-right-radius: 2px;padding: 2px 10px;`,
+        );
+    }
+
     static error(params: ICatch) {
         let _message: string = '{ type: "empty", message: "empty"}';
         if (isObject(params) && !isEmpty(params)) {
             _message = JSON.stringify(params);
         }
+        Logger.log("error", params.type, _message)
         throw new Error(_message);
     }
 
 
+    static info(params: IMessage) {
+        let _message: string = '{ type: "empty", message: "empty"}';
+        if (isObject(params) && !isEmpty(params)) {
+            _message = JSON.stringify(params);
+        }
+        Logger.log("info", params.type, _message)
+    }
 }
+
